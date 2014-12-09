5cacde20236a80363eda663521a8c53a9b352230
toshiba011 <gtoshiba011@gmail.com>
2014-12-09 21:07:18 +0800

    [CPH] add android web socket

--- src/com/example/justask/MainActivity.java
+++ src/com/example/justask/MainActivity.java
@@ -13,6 +13,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.os.Build;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
 	/**
@@ -158,5 +168,54 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
 					ARG_SECTION_NUMBER));
 		}
 	}
+	
+	
+	// socket communication start
+	private WebSocketClient mWebSocketClient;
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://websockethost:8080");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
 
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //TextView textView = (TextView)findViewById(R.id.messages);
+                        //textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+    public void sendMessage(View view) {
+        //EditText editText = (EditText)findViewById(R.id.message);
+        //mWebSocketClient.send(editText.getText().toString());
+        //editText.setText("");
+    }
+    // end of socket communication
 }
