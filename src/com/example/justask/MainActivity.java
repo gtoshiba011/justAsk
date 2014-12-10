462963207368e64a2cf68c7c67987d62186caa79
HKY <bhb811340@gmail.com>
2014-12-10 14:37:20 +0800

    Drawer Layout With Tabs function complete.

--- src/com/example/justask/MainActivity.java
+++ src/com/example/justask/MainActivity.java
@@ -1,175 +1,148 @@
 package com.example.justask;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
-import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.os.Build;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.TextView;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 
-	/**
-	 * Fragment managing the behaviors, interactions and presentation of the
-	 * navigation drawer.
-	 */
-	private NavigationDrawerFragment mNavigationDrawerFragment;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
 
-	/**
-	 * Used to store the last screen title. For use in
-	 * {@link #restoreActionBar()}.
-	 */
-	private CharSequence mTitle;
+public class MainActivity extends SherlockFragmentActivity {
+
+	private DrawerLayout mDrawerLayout;
+	private LinearLayout mDrawer;
+	private ActionBarDrawerToggle mDrawerToggle;
+	
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
+
+	// Tabs titles
+	private String[] tabsTitles = {"Profile", "Questions", "Survey"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
-				.findFragmentById(R.id.navigation_drawer);
-		mTitle = getTitle();
+		// Drawer
+		mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+		mDrawer = (LinearLayout)findViewById(R.id.drawer);
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 
-		// Set up the drawer.
-		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
-				(DrawerLayout) findViewById(R.id.drawer_layout));
-/*	
-		FragmentTabHost tabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
-	    tabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
-	    tabHost.addTab(tabHost.newTabSpec("Profile").setIndicator("Profile"), ProfileForSpeaker.class, null);
-	    tabHost.addTab(tabHost.newTabSpec("Questions").setIndicator("Questions"), QuestionsForSpeaker.class, null);
-	    tabHost.addTab(tabHost.newTabSpec("Survey").setIndicator("Survey"), SurveyForSpeaker.class, null);
-//*/
-	}
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-	/**************************
-	* 
-	* µπ§l≠∂≈“©I•s•Œ
-	* 
-	**************************/
-	public String getProfileData(){
-		return "Profile Page";
-	}
-	public String getQuestionsData(){
-		return "Question Page";
-	}
-	public String getSurveyData(){
-		return "Survey Page";
-	}
-
-	@Override
-	public void onNavigationDrawerItemSelected(int position) {
-		// update the main content by replacing fragments
-		FragmentManager fragmentManager = getFragmentManager();
-		fragmentManager
-				.beginTransaction()
-				.replace(R.id.container,
-						PlaceholderFragment.newInstance(position + 1)).commit();
-	}
-
-	public void onSectionAttached(int number) {
-		switch (number) {
-		case 1:
-			mTitle = getString(R.string.title_section1);
-			break;
-		case 2:
-			mTitle = getString(R.string.title_section2);
-			break;
-		case 3:
-			mTitle = getString(R.string.title_section3);
-			break;
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,
+				mDrawerLayout,
+				R.drawable.ic_drawer,
+				R.string.drawer_open,
+				R.string.drawer_close) {
+			
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+			}
+			
+			public void onDrawerOpened(View drawerView) {
+				// Set the title on the action when drawer open
+				getSupportActionBar().setTitle("JustAsk");
+				super.onDrawerOpened(drawerView);
+			}
+		};
+		
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		// Initialization
+		final TabHost tabHost=(TabHost)findViewById(android.R.id.tabhost);
+		tabHost.setup();
+		
+		for (int i = 0; i < tabsTitles.length; i++) {
+			String tabName = tabsTitles[i];
+			TabHost.TabSpec spec=tabHost.newTabSpec(tabName);
+			spec.setContent(R.id.fakeTabContent);
+			spec.setIndicator(tabName);
+			tabHost.addTab(spec);
 		}
-	}
 
-	public void restoreActionBar() {
-		ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-		actionBar.setDisplayShowTitleEnabled(true);
-		actionBar.setTitle(mTitle);
-	}
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getSupportActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager(), tabsTitles.length);
+		viewPager.setAdapter(mAdapter);
+		tabHost.setOnTabChangedListener(new OnTabChangeListener() {
+			
+			@Override
+			public void onTabChanged(String tabId) {
+				for (int i = 0; i < tabsTitles.length; i++) {
+					if (tabId.equals(tabsTitles[i])) {
+						viewPager.setCurrentItem(i);
+						break;
+					}
+				}
+			}
+		});
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		if (!mNavigationDrawerFragment.isDrawerOpen()) {
-			// Only show items in the action bar relevant to this screen
-			// if the drawer is not showing. Otherwise, let the drawer
-			// decide what to show in the action bar.
-			getMenuInflater().inflate(R.menu.main, menu);
-			restoreActionBar();
-			return true;
-		}
-		return super.onCreateOptionsMenu(menu);
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position) {
+				tabHost.setCurrentTab(position);
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		if (item.getItemId() == android.R.id.home) {
+			if (mDrawerLayout.isDrawerOpen(mDrawer)) {
+				mDrawerLayout.closeDrawer(mDrawer);
+			} else {
+				mDrawerLayout.openDrawer(mDrawer);
+			}
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-
-		@Override
-		public void onAttach(Activity activity) {
-			super.onAttach(activity);
-			((MainActivity) activity).onSectionAttached(getArguments().getInt(
-					ARG_SECTION_NUMBER));
-		}
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
 	}
 	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggles
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
 	
+	/*
 	// socket communication start
 	private WebSocketClient mWebSocketClient;
     private void connectWebSocket() {
@@ -217,5 +190,5 @@ public class MainActivity extends Activity implements NavigationDrawerFragment.N
         //mWebSocketClient.send(editText.getText().toString());
         //editText.setText("");
     }
-    // end of socket communication
+    // end of socket communication*/
 }
