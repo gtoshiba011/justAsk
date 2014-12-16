0e96a886dffd272e10cb30b74f5dde5f696b8ae8
toshiba011 <gtoshiba011@gmail.com>
2014-12-16 11:56:12 +0800

    [CPH] add "import android.util.Log;"

--- src/com/example/justask/MainActivity.java
+++ src/com/example/justask/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
+import android.util.Log;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
